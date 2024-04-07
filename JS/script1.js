f4c8c86f937e43f25ded4e21f8d2db77113b25de
Bieelogrom <gabrielamorim15436@gmail.com>
2024-04-07 11:24:16 -0300

    1.2

---
+++ JS/script1.js
@@ -0,0 +1,9 @@
+
+
+var valor = parseInt(prompt("Digite o valor desejado: "))
+
+var valorAnterior = valor - 1;
+
+
+alert("O antecessor de "+ (valor) + " é " + (valorAnterior))
+window.location.href = "index.html";
---
+++ JS/script2.js
@@ -0,0 +1,10 @@
+
+var base = parseFloat(prompt("Digite o valor da base: "))
+var altura = parseFloat(prompt("Digite o valor da altura: "))
+
+var area = base * altura;
+
+alert("O valor da área é de "+ (area))
+
+
+window.location.href = "index.html";
